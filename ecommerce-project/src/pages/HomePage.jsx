bf508fdcda1d3945e58041ffd8933a4795e5185a
Ravindu Nethmina <Ravindu.nethmina.work@gmail.com>
2025-12-01 17:55:18 +0530

    Fetch Data from the backend

--- ecommerce-project/src/pages/HomePage.jsx
+++ ecommerce-project/src/pages/HomePage.jsx
@@ -4,6 +4,12 @@ import { products } from "../../starting-code/data/products";
 import "./HomePage.css";
 
 function HomePage() {
+  fetch("http://localhost:3000/api/products").then((response) => {
+    response.json().then((data) => {
+      console.log(data);
+    });
+  });
+
   return (
     <>
       <title>HomePage</title>
@@ -14,10 +20,7 @@ function HomePage() {
             return (
               <div key={product.id} className="product-container">
                 <div className="product-image-container">
-                  <img
-                    className="product-image"
-                    src={product.image}
-                  />
+                  <img className="product-image" src={product.image} />
                 </div>
 
                 <div className="product-name limit-text-to-2-lines">
@@ -27,14 +30,18 @@ function HomePage() {
                 <div className="product-rating-container">
                   <img
                     className="product-rating-stars"
-                    src={`images/ratings/rating-${product.rating.stars * 10}.png`}
+                    src={`images/ratings/rating-${
+                      product.rating.stars * 10
+                    }.png`}
                   />
                   <div className="product-rating-count link-primary">
                     {product.rating.count}
                   </div>
                 </div>
 
-                <div className="product-price">${(product.priceCents / 100).toFixed(2)}</div>
+                <div className="product-price">
+                  ${(product.priceCents / 100).toFixed(2)}
+                </div>
 
                 <div className="product-quantity-container">
                   <select>
